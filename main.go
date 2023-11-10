3d260cfd0d754108af0fb792369797d3cf7eedff
Mochammad Hanif <mochanifz@gmail.com>
2023-11-10 11:22:35 +0700

    Update main.go

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"back-end-golang/configs"
 	_ "back-end-golang/docs"
 	"back-end-golang/routes"
-	"log"
+	// "log"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -63,11 +63,11 @@ func main() {
 	routes.Init(e, db)
 
 	e.GET("/swagger/*", echoSwagger.WrapHandler)
-	// e.Logger.Fatal(e.Start(":80"))
+	e.Logger.Fatal(e.Start(":80"))
 
 	// Start server with TLS
-	err = e.StartTLS(":443", "/etc/nginx/ssl/fullchain.pem", "/etc/nginx/ssl/privkey.pem")
-	if err != nil {
-		log.Fatal("StartTLS: ", err)
-	}
+	// err = e.StartTLS(":443", "/etc/nginx/ssl/fullchain.pem", "/etc/nginx/ssl/privkey.pem")
+	// if err != nil {
+		//log.Fatal("StartTLS: ", err)
+	//}
 }
