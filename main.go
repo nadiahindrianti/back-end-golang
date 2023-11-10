25f4656e3098ec95224204aa234f09359ab61483
Mochammad Hanif <mochanifz@gmail.com>
2023-11-10 10:21:53 +0700

    Update main.go

--- main.go
+++ main.go
@@ -63,11 +63,11 @@ func main() {
 	routes.Init(e, db)
 
 	e.GET("/swagger/*", echoSwagger.WrapHandler)
-	e.Logger.Fatal(e.Start(":80"))
+	// e.Logger.Fatal(e.Start(":80"))
 
 	// Start server with TLS
-	// err = e.StartTLS(":443", "/etc/nginx/ssl/fullchain.pem", "/etc/nginx/ssl/privkey.pem")
-	// if err != nil {
-	//	log.Fatal("StartTLS: ", err)
-	//}
+	err = e.StartTLS(":443", "/etc/nginx/ssl/fullchain.pem", "/etc/nginx/ssl/privkey.pem")
+	if err != nil {
+		log.Fatal("StartTLS: ", err)
+	}
 }
