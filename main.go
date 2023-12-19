16dc6b745f9c8a27a5ae443f657cecef35c888d5
Mochammad Hanif <mochanifz@gmail.com>
2023-12-19 13:47:14 +0700

    Update main.go

--- main.go
+++ main.go
@@ -66,7 +66,7 @@ func main() {
 	// e.Logger.Fatal(e.Start(":443"))
 
 	// Start server with TLS
-	err = e.StartTLS(":443", "/etc/letsencrypt/live/api.tripease.my.id/fullchain.pem", "/etc/letsencrypt/live/api.tripease.my.id/privkey.pem")
+	err = e.StartTLS(":443", "/etc/nginx/ssl/fullchain.pem", "/etc/nginx/ssl/privkey.pem")
 	if err != nil {
 		log.Fatal("StartTLS: ", err)
 	}
